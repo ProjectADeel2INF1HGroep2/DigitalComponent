6747b3f4dd2f1815c5236f80a6ed8d48e0780978
TheDenizen <25197071+TheDenizen@users.noreply.github.com>
2018-11-26 21:08:22 +0100

    2 buttons now change color when being hovered over

--- DigitalComponent.py
+++ DigitalComponent.py
@@ -5,7 +5,7 @@ from pygame.locals import*
 pygame.init()
 
 display_height = 800
-display_width = 1000
+display_width = 1100
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Turf Wars')
 clock = pygame.time.Clock()
@@ -15,13 +15,18 @@ resource_path = os.path.join(current_path, 'resources') # The resource folder pa
 image_path = os.path.join(resource_path, 'images') # The image folder path
 
 
+DEFAULT_FONT = "freesansbold.ttf"
+LARGE_FONT  = pygame.font.Font(os.path.join("resources", "fonts", "COMIC.ttf"),80)
 
 
-blue = (63,72,204)
+blue = (0,0,200)
 red = (200,0,0)
-black = (0,0,0)
 green = (0,200,0)
+black = (0,0,0)
 white = (255,255,255)
+bright_red = (255,0,0)
+bright_blue = (0,0,255)
+bright_green = (255,0,0)
 
 
 logoImg = pygame.image.load(os.path.join(image_path, 'logo.png'))
@@ -31,7 +36,20 @@ def logo(x,y):
 
 x = (display_width * 0.10)
 y = (display_height * 0.10)
+    
+def exit():
+    pygame.quit()
+    quit()
 
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    TextSurf, TextRect = text_objects(text, LARGE_FONT)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
 
 def game_intro():
 
@@ -46,14 +64,24 @@ def game_intro():
         
         gameDisplay.fill(white)
         logo(x, y)
-        # largeText = pygame.font.Font('freesansbold.ttf',115)
-        # TextSurf, TextRect = text_objects("Turf Wars", largeText)
-        # TextRect.center = ((display_width/2),(display_height/2))
-        # gameDisplay.blit(TextSurf, TextRect)
+        TextSurf, TextRect = text_objects("Digital Component", LARGE_FONT)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        mouse = pygame.mouse.get_pos()
+        if 100+300 > mouse[0] > 100 and 550+150 > mouse[1] > 550:
+            pygame.draw.rect(gameDisplay, bright_blue,(100,550,300,150))
+        else:
+            pygame.draw.rect(gameDisplay, blue,(100,550,300,150))
+
+        if 400+300 > mouse[0] > 400 and 550+150 > mouse[1] > 550:
+            pygame.draw.rect(gameDisplay, bright_red,(500,550,300,150))
+        else:
+            pygame.draw.rect(gameDisplay, red,(500,550,300,150))
+
         pygame.display.update()
         clock.tick(15)
         
-
 game_intro()
 pygame.quit()
 quit()
