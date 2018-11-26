03c83f316f672522298297a25c072c19b6932c3f
TheDenizen <25197071+TheDenizen@users.noreply.github.com>
2018-11-26 23:32:28 +0100

    laatste dan

--- DigitalComponent.py
+++ DigitalComponent.py
@@ -53,7 +53,7 @@ def message_display(text):
     gameDisplay.blit(TextSurf, TextRectangle)
 
 
-def button(msg, x,y,w,h,ic,ac):
+def button(msg, x,y,w,h,ic,ac,action=None):
 # This function has the parameters of:
 # msg: What do you want the button to say on it.
 # x: The x location of the top left coordinate of the button box.
@@ -62,15 +62,29 @@ def button(msg, x,y,w,h,ic,ac):
 # h: Button height.
 # ic: Inactive color (when a mouse is not hovering).
 # ac: Active color (when a mouse is hovering).
+
     mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+                action()      
     else:
         pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
 
     textSurf, textRectangle = text_objects(msg, SMALL_FONT)
     textRectangle.center = ( (x+(w/2)), (y+(h/2)) )
     gameDisplay.blit(textSurf, textRectangle)
+
+
+def gameloop():
+        
+    gameDisplay.fill(white)
+    button('EXIT!', 550,550,300,150,red,bright_red,exit)
+
+
 
 def game_intro():
 
@@ -91,12 +105,13 @@ def game_intro():
 
 
 
-        button('START!',150,550,300,150,bright_blue,blue)
-        button('EXIT!', 550,550,300,150,red,bright_red)
+        button('START!',150,550,300,150,bright_blue,blue,gameloop)
+        button('EXIT!', 550,550,300,150,red,bright_red,exit)
 
         pygame.display.update()
         clock.tick(15)
-        
+
+
 game_intro()
 pygame.quit()
 quit()
